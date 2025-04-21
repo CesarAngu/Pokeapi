e49040211c6adc6483a68471ac08617ca64c7309
CesarAngu <130942750+CesarAngu@users.noreply.github.com>
2025-04-21 19:46:33 +0000

    Tu mensaje de commit aquí

---
+++ src/lista/index.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useContext } from 'react'
+import { AppContext } from '../../contexto/contexto';
+import { useNavigate } from "react-router-dom";
+import './style.css'
+import filtro from '../filtro'
+
+function Lista() {
+  
+  const [data, setData] = useState([]);
+  const [tipoSeleccionado, setTipoSeleccionado] = useState('All');
+
+  useEffect(() => {
+
+   
+    const obtenerDatos = async () => {
+      if (tipoSeleccionado === 'All') {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+        const json = await res.json();
+        setData(json.results);
+      } else {
+        const res = await fetch(`https://pokeapi.co/api/v2/type/${tipoSeleccionado}`);
+        const json = await res.json();
+        const listaFiltrada = json.pokemon.map(p => p.pokemon);
+        setData(listaFiltrada);
+      }
+    };
+
+    obtenerDatos();
+  }, [tipoSeleccionado]);
+
+  const handleTipoChange = (tipo) => {
+    setTipoSeleccionado(tipo);
+  };
+
+  return (
+    <>
+    
+    <Filtro onTipoChange={handleTipoChange} />
+    <section className='c-lista'>
+
+      {resultados.map((pokemon, index) => (
+        <div className='c-lista-pokemon'
+        onClick={() => navigate(`/detalle/${pokemon.name}`)}
+        key={index}>
+          <p>{pokemon.url.split("/")[6]}</p>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`} 
+                alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
+              />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+    </section>
+    </>
+  );
+}
+  
+export default Lista
